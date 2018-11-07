d9b44ba436b9724e89aeff908e76e0afffb1302b
Dan <dpedersen84@gmail.com>
2018-11-07 15:18:28 -0600

    react router added

---
+++ client/src/pages/Trips/Trips.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Trips extends Component {
+    render() {
+        return (
+            <h1>
+                Trips
+            </h1>
+        )
+    }
+};
+
+export default Trips;
